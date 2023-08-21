41ca1e1c3b7f14cbc1ab018ec2dec7284b8c555d
Mehedi Hasan <140379801+mehedihasan22201109@users.noreply.github.com>
2023-08-21 21:47:18 +0600

    Add files via upload

--- lab assignment pblm 007.c
+++ lab assignment pblm 007.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+void printFirstNEvenNumbers(int n)
+{
+    int i;
+    printf("Even Numbers: ");
+    for ( i = 1; i <= n; i++)
+    {
+        printf("%d", 2 * i);
+        if (i < n)
+        {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+
+    printf("n = ");
+    scanf("%d", &n);
+
+    printFirstNEvenNumbers(n);
+
+    return 0;
+}
