655b6019830f93795e5c088f292c1320bf6e8b0e
Mehedi Hasan <140379801+mehedihasan22201109@users.noreply.github.com>
2023-08-21 21:45:04 +0600

    Add files via upload

--- lab assignment pblm 05.c
+++ lab assignment pblm 05.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int isVowel(char ch) {
+    ch = tolower(ch);
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+char getCharacter() {
+    char character;
+    printf("Enter a character: ");
+    scanf(" %c", &character);
+    return character;
+}
+
+int main() {
+    char character = getCharacter();
+
+    if (isVowel(character)) {
+        printf("Vowel\n", character);
+    } else {
+        printf("Consonant\n", character);
+    }
+
+    return 0;
+}
