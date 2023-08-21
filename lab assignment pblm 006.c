3d5637639e88f0bf8b2192ca74e9a490c5831091
Mehedi Hasan <140379801+mehedihasan22201109@users.noreply.github.com>
2023-08-21 21:46:46 +0600

    Add files via upload

--- lab assignment pblm 006.c
+++ lab assignment pblm 006.c
@@ -1,1 +1,21 @@
+##include <stdio.h>
 
+float GetTriangleArea(float base, float height) {
+    return ((1 * base * height)/2);
+}
+
+int main() {
+    float base, height;
+
+    printf("base = ");
+    scanf("%f", &base);
+
+    printf("height = ");
+    scanf("%f", &height);
+
+    float area = GetTriangleArea(base, height);
+
+    printf("Area = %.0f\n", area);
+
+    return 0;
+}
